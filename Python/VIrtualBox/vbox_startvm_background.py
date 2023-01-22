ad31e3d38b78eb5ebb07cc3621a53c945d238bc2
motoi <goodddd6@gmail.com>
2023-01-22 12:43:01 +0900

    create virtualbox

---
+++ Python/VIrtualBox/vbox_startvm_background.py
@@ -0,0 +1,11 @@
+import subprocess
+
+# Variable
+vm_name1 = "alma9_node1"
+cmd = 'C:\Program Files\Oracle\VirtualBox\VBoxManage.exe startvm "{}" --type headless'.format(vm_name1)
+
+# CommandLine
+print("execute following command : " + cmd)
+
+# execute command
+subprocess.run(cmd)
