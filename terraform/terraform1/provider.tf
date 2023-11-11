ebbb862e41ecfcdea900af3820f3c3d2b49b0471
motoi <goodddd6@gmail.com>
2023-11-11 13:22:05 +0900

    add terraform

---
+++ terraform/terraform1/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.78.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
+
+resource "azurerm_application_insights" "example" {
+  name                = "tf-test-appinsights"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  application_type    = "web"
+}
---
+++ terraform/terramodule/VM.tf
@@ -0,0 +1,47 @@
+resource "azurerm_resource_group" "terraform-test-rg" {
+  name     = "terraform-rg"
+  location = "eastus2"
+}
+resource "azurerm_virtual_network" "terraform-test-vnet" {
+  name                = "terraform-vent"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.terraform-test-rg.location
+  resource_group_name = azurerm_resource_group.terraform-test-rg.name
+}
+resource "azurerm_subnet" "terraform-test-subnet" {
+  name                 = "terraform-internal"
+  resource_group_name  = azurerm_resource_group.terraform-test-rg.name
+  virtual_network_name = azurerm_virtual_network.terraform-test-vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+resource "azurerm_network_interface" "terraform-test-nif" {
+  name                = "terraform-test-vm-nic"
+  location            = azurerm_resource_group.terraform-test-rg.location
+  resource_group_name = azurerm_resource_group.terraform-test-rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.terraform-test-subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_windows_virtual_machine" "terraform-test-vm" {
+  name                = "terra-testvm-1"
+  resource_group_name = azurerm_resource_group.terraform-test-rg.name
+  location            = azurerm_resource_group.terraform-test-rg.location
+  size                = "Standard_B2ms"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.terraform-test-nif.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
