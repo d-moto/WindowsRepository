6a43fd59e630bd69933d2c7a9a58a0e9f48fb8b6
motoi-daichi <goodddd6@gmail.com>
2023-12-19 21:40:25 +0900

    add bastion.tf

---
+++ terraform/bastion/bastion.tf
@@ -0,0 +1,25 @@
+resource "azurerm_public_ip" "example" {
+  name                = "examplepip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "example" {
+  name                = "examplebastion"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  
+  file_copy_enabled   = true
+  sku                 = Standard
+  tunneling_enabled   = true
+  
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.example.id
+    public_ip_address_id = azurerm_public_ip.example.id
+    allocation_method    = "Dynamic"
+  }
+}
