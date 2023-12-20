d89789d05da68348910152dc9ddecf919b60bb50
motoi-daichi <goodddd6@gmail.com>
2023-12-20 19:46:11 +0900

    modify bastion for terraform apply

--- terraform/bastion/bastion.tf
+++ terraform/bastion/bastion.tf
@@ -1,25 +1,59 @@
-resource "azurerm_public_ip" "example" {
-  name                = "examplepip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg"
+  location = "japaneast"
+}
+
+resource "azurerm_virtual_network" "vn" {
+  name                = "vn"
+  address_space       = ["172.30.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "sbn" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefixes     = ["172.30.1.0/24"]
+}
+
+## for bastion
+resource "azurerm_public_ip" "pubip" {
+  name                = "pubip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  #allocation_method   = "Dynamic"
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
-resource "azurerm_bastion_host" "example" {
-  name                = "examplebastion"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_bastion_host" "bashost" {
+  name                = "bashost"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   
   file_copy_enabled   = true
-  sku                 = Standard
+  sku                 = "Standard"
   tunneling_enabled   = true
   
 
   ip_configuration {
     name                 = "configuration"
-    subnet_id            = azurerm_subnet.example.id
-    public_ip_address_id = azurerm_public_ip.example.id
-    allocation_method    = "Dynamic"
+    subnet_id            = azurerm_subnet.sbn.id
+    public_ip_address_id = azurerm_public_ip.pubip.id
   }
 }
